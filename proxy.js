ba6c75eaf2e094002f2adf7776ab31719265e9a4
Robert Pfaff <robert@robertpfaff.net>
2026-02-22 18:17:56 -0500

    fix auth.js 2

--- proxy.js
+++ proxy.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function proxy(request) {
   const session = await auth();
-  
+
   if (!session) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
